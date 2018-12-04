388fbde126340fa076e07502064eb7d38cf953e6
UnreasonableMrT <wo798773611@outlook.com>
2018-12-04 22:07:58 +0800

    提交文件

---
+++ Network/Conn.java
@@ -0,0 +1,51 @@
+package Network;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Conn {
+	//缓冲区长度
+	public final int BUFFER_SIZE = 1024;
+	//储存套接字
+	public Socket socket;
+	//这个conn是否被使用了
+	public boolean isUse = false;
+	//读缓冲区
+	public byte[] readBuffer = new byte[BUFFER_SIZE];
+	//缓冲区已使用长度标记
+	public int buffCount = 0;
+	//沾包处理，计算信息占用缓冲区的长度
+	public byte[] lenBytes = new byte[4];
+	//public User user;
+	
+	public Conn(){
+		//初始化缓冲区;
+		readBuffer = new byte[BUFFER_SIZE];
+	}
+	//初始化
+	public void Init(Socket socket) {
+		this.socket = socket;
+		isUse = true;
+		buffCount = 0;
+	}
+	//缓冲区剩余空间
+	public int BuffRemain() {
+		return BUFFER_SIZE - buffCount;
+	}
+	
+	public String GetAdress() {
+		if(!isUse)
+			return "无法获取地址";
+		return socket.getInetAddress().toString();
+	}
+	
+	public void Close() throws IOException {
+		if(!isUse)
+			return;
+		//if(user != null){}
+		System.out.println("[断开连接]:"+ GetAdress());
+		socket.close();
+		isUse = false;
+	}
+}
---
+++ Network/ServNet.java
@@ -0,0 +1,89 @@
+package Network;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ServNet {
+
+	public Socket socket;
+	public ServerSocket serverSocket;
+	public static Conn[] conns;
+	public static int maxConn = 50;
+	//单例
+	public static ServNet instance;
+	public ServNet() {
+		instance = this;
+	}
+	
+	//对象池索引，寻找可使用的conn
+	public int NewIndex() {
+		if(conns == null) {
+			return -1;
+		}
+		for(int i = 0; i < conns.length; i++) {
+			if(conns[i] == null) {
+				conns[i] = new Conn();
+				return i;
+			}else if(conns[i].isUse == false) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	public static void main(String[] args) throws IOException {
+		ServNet servNet = new ServNet();
+		servNet.Start(6000);
+		
+	}
+	
+	public void Start(int port) throws IOException {
+		//初始化连接池
+		conns = new Conn[maxConn];
+		//初始化连接池
+		for(int i = 0; i < maxConn; i++) {
+			conns[i] = new Conn();
+		}
+		
+		serverSocket =new ServerSocket(port);
+		Accept();
+	}
+	
+	public void Accept() throws IOException {
+		ExecutorService threadPool = Executors.newFixedThreadPool(100);
+		while(true){
+			Runnable runnable=()->{
+			try {
+					Socket socket = serverSocket.accept();
+					int index = NewIndex();
+					if(index < 0) {
+						socket.close();
+						System.out.println("连接已满");
+						return;
+					}
+					Conn conn = conns[index];
+					conn.Init(socket);
+					String str = conn.GetAdress();
+					System.out.println("客户端连接 ["+str+"]");
+		    		// 建立好连接后，从socket中获取输入流，并建立缓冲区进行读取
+		      /*      InputStream inputStream = socket.getInputStream();
+		            StringBuilder strb = new StringBuilder();
+		            while ((conn.buffCount = inputStream.read(conn.readBuffer)) != -1) {
+		            	strb.append(new String(conn.readBuffer, 0, conn.buffCount, "UTF-8"));
+		            }
+		            System.out.println("get message from client: " + strb);
+		            inputStream.close();*/
+		            socket.close();
+		            } catch (Exception e) {
+		            e.printStackTrace();
+		            }
+		    	};
+		        threadPool.submit(runnable);
+		}
+	 }
+}
+
